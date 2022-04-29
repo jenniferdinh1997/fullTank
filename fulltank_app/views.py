beaca7cc8484adef69e012bca7eac2496ea2f07f
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2022-04-29 11:05:00 -0700

    addingAPP

--- fulltank_app/views.py
+++ fulltank_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>HOME PAGE!!!</h1>')
+
 # Create your views here.
