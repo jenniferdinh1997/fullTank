4b9cc9b940ff6a2f2513f0d6581ddd591b52d0e2
elroy-He <elroyhsc@gmail.com>
2022-05-01 19:21:50 -0700

    signup works

--- fulltank_app/urls.py
+++ fulltank_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('stations/<int:pk>/delete/', views.StationDelete.as_view(), name='stations_delete'),
     path('stations/<int:station_id>/', views.stations_detail, name='detail'),
     path('stations/<int:pk>/update/', views.PriceUpdate.as_view(), name='price_update'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
