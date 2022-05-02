bb0547e8b1d7d81d76e6e5553c6d0d140947f7be
OlloClark <ollo42@hotmail.co.uk>
2022-05-01 18:45:27 -0700

    adding valid form station create2

--- fulltank_app/views.py
+++ fulltank_app/views.py
@@ -14,7 +14,7 @@ class StationCreate(CreateView):
   model = Station
   fields = ['name', 'company', 'date', 'price', 'cards_accepted', 'zipcode']
 
-   def form_valid(self, form):
+  def form_valid(self, form):
     form.instance.user = self.request.user
     return super().form_valid(form)
 
